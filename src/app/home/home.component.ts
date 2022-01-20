1643a1e5147bf20315210251067bc4918e4f66ae
HanaHentu <zubrzyckid96@gmail.com>
2022-01-20 16:36:15 +0100

    home login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../navbar.service';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,13 @@ export class HomeComponent implements OnInit {
 
   visible: boolean = false;
 
-  constructor( public navbarService: NavbarService ) {}
-
- 
+  constructor( public navbarService: NavbarService ) {} 
 
   ngOnInit(): void {
     this.navbarService.hide();
   }
+
+  faEnvelope = faEnvelope;
+  faLock = faLock;
 
 }
